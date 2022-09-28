44e777b964eaefc641b78f8c807de6d72cd8c426
kennbeckloff <kennbeckloff688@gmail.com>
2022-09-28 23:07:52 +0300

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    #validate All posts have a title
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbait
+    def title_is_clickbait
+        if title && !title.include?("Won't Believe")
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
+
+
 end
